c2a1448f5afd01b892ec469a665a4777db8f4b15
AlexeyZubkov78 <104060082+AlexeyZubkov78@users.noreply.github.com>
2022-08-26 12:50:23 +0300

    Add files via upload

---
+++ srvbook/Controllers/BookLibraryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace srvbook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookLibraryController : ControllerBase
+    {
+        private readonly ILogger<BookLibraryController> _logger;
+
+        public BookLibraryController(ILogger<BookLibraryController> logger)
+        {
+            _logger = logger;
+        }
+
+      //  [HttpGet]
+        // public IEnumerable<Data> GetBookList()
+        //      {
+
+        //      }
+
+      //  [HttpPut]
+
+    }
+}
---
+++ srvbook/Data.cs
@@ -0,0 +1,44 @@
+﻿namespace srvbook
+{
+    public class Data
+    {
+        public static List<string> Readers { get; set; } = new List<string>()
+        {
+            "Арбузов Н.В.",
+            "Вылегжанина О.Ю.",
+            "Иванов П.Р.",
+            "Петров С.Е.",
+            "Сидоров А.И"
+        };
+
+        public static List<string> Books { get; set; } = new List<string>()
+     {
+       "Достоевский \"Идиот\"",
+       "Куприн \"Сборник рассказов\"",
+       "Лермонтов \"Мцыри\"",
+       "Толстой \"Война и мир\""
+     };
+
+        public Dictionary<string, List<string>> Info { get; set; } = new Dictionary<string, List<string>>();
+
+        public List<string> GetBooks()
+        {
+            return Books;
+        }
+        public List<string> GetReaders()
+        {
+            return Readers;
+        }
+     public List<string> GetbooksGivenReader(string nameReader)
+        {
+          var results = new List<string>();
+            var isReader = Info.ContainsKey(nameReader);
+            if (isReader) results = Info[nameReader];
+            return results;
+        }
+
+    
+    
+    
+    }
+}
